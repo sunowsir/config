b642ced74a23ec210f1481d85bc19b21bd1119d5
sunowsir <sunow.wang@gmail.com>
2021-03-05 12:47:00 +0800

    [update] patchd status color

--- dwm/config.h
+++ dwm/config.h
@@ -15,7 +15,7 @@ static const unsigned int gappoh    = 10;       /* horiz outer gap between windo
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=16" };
 static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=16";
@@ -31,6 +31,7 @@ static const char selfgcolor[]      = "#ffffff"; // "#eeeeee";
 
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
+static const unsigned int statusalpha = 0xd0;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
