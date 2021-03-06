2f2509a5964a185fe3bd89fd541723768663c1b0
sunowsir <sunow.wang@gmail.com>
2021-03-06 18:17:23 +0800

    [change] change start script

--- autostart.dwm/dwmbar-functions/dwm_battery.sh
+++ autostart.dwm/dwmbar-functions/dwm_battery.sh
@@ -12,12 +12,12 @@ dwm_battery () {
     printf "%s" "$SEP1"
     if [ "$IDENTIFIER" = "unicode" ]; then
         if [ "$STATUS" = "Discharging" ]; then
-            printf "🔋 %s%% " "$CHARGE" 
+            printf "🔋 %s%%" "$CHARGE" 
         else
-            printf "🔌 %s%% " "$CHARGE"
+            printf "🔌 %s%%" "$CHARGE"
         fi
     else
-        printf "BAT %s%% %s" "$CHARGE" "$STATUS"
+        printf "BAT %s%%" "$CHARGE" 
     fi
     printf "%s\n" "$SEP2"
 }
