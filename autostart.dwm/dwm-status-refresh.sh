e545f06b9967c79f06b6ebeec1d2c48ea7a86180
sunowsir <sunow.wang@gmail.com>
2021-03-09 11:13:02 +0800

    [change] change status script.

--- autostart.dwm/dwm-status-refresh.sh
+++ autostart.dwm/dwm-status-refresh.sh
@@ -8,6 +8,15 @@
 
 LOC=$(readlink -f "$0")
 DIR=$(dirname "$LOC")
+DSR_Col=(
+    "#000000"
+    "#8B8989"
+    "#fff929"
+    "#c20000"
+    "#8A2BE2"
+    "#00FA9A"
+)
+
 export IDENTIFIER="unicode"
 
 # shellcheck source=dwmbar-functions/dwm_transmission.sh
@@ -25,30 +34,27 @@ source "$DIR/dwmbar-functions/dwm_date.sh"
 # shellcheck source=dwmbar-functions/dwm_memory.sh
 source "$DIR/dwmbar-functions/dwm_memory.sh"
 
-# source "$DIR/dwmbar-functions/dwm_cmus.sh"
-# source "$DIR/dwmbar-functions/dwm_resources.sh"
-# source "$DIR/dwmbar-functions/dwm_mail.sh"
-# source "$DIR/dwmbar-functions/dwm_backlight.sh"
-# source "$DIR/dwmbar-functions/dwm_pulse.sh"
-# source "$DIR/dwmbar-functions/dwm_weather.sh"
-# source "$DIR/dwmbar-functions/dwm_vpn.sh"
-# source "$DIR/dwmbar-functions/dwm_network.sh"
-# source "$DIR/dwmbar-functions/dwm_keyboard.sh"
-# source "$DIR/dwmbar-functions/dwm_ccurse.sh"
+function gen_block_str() {
+    # 文本颜色：^cRGB颜色值^
+    # 背景颜色：^bRGB颜色值^
+    echo -ne "^b${1}^^c${2}^${3}"
+}
 
+function gen_str() {
+    local dwm_status=""
+    dwm_status="${dwm_status}$(gen_block_str "${DSR_Col[2]}" "${DSR_Col[1]}" " \ue0b0")"
+    dwm_status="${dwm_status}$(gen_block_str "${DSR_Col[2]}" "${DSR_Col[0]}" " $(dwm_memory) ")"
+    dwm_status="${dwm_status}$(gen_block_str "${DSR_Col[3]}" "${DSR_Col[2]}" "\ue0b0")"
+    dwm_status="${dwm_status}$(gen_block_str "${DSR_Col[3]}" "${DSR_Col[0]}" " $(dwm_transmission) ")"
+    dwm_status="${dwm_status}$(gen_block_str "${DSR_Col[4]}" "${DSR_Col[3]}" "\ue0b0")"
+    dwm_status="${dwm_status}$(gen_block_str "${DSR_Col[4]}" "${DSR_Col[0]}" " $(dwm_alsa) ")"
+    dwm_status="${dwm_status}$(gen_block_str "${DSR_Col[5]}" "${DSR_Col[4]}" "\ue0b0")"
+    dwm_status="${dwm_status}$(gen_block_str "${DSR_Col[5]}" "${DSR_Col[0]}" " $(dwm_battery) ")"
+    dwm_status="${dwm_status}$(gen_block_str "${DSR_Col[1]}" "${DSR_Col[5]}" "\ue0b0")"
+    dwm_status="${dwm_status}$(gen_block_str "${DSR_Col[1]}" "${DSR_Col[0]}" " $(dwm_date) ")"
 
-# 文本颜色：^cRGB颜色值^
-# 背景颜色：^bRGB颜色值^
-xsetroot -name " \
-^c#8B8989^^b#fff929^$(echo -e "\ue0b0")\
-^b#fff929^^c#000000^ $(dwm_memory) \
-^c#fff929^^b#c20000^$(echo -e "\ue0b0") \
-^c#000000^^b#c20000^$(dwm_transmission) \
-^c#c20000^^b#8A2BE2^$(echo -e "\ue0b0") \
-^c#000000^^b#8A2BE2^$(dwm_alsa) \
-^c#8A2BE2^^b#00FA9A^$(echo -e "\ue0b0") \
-^c#000000^^b#00FA9A^$(dwm_battery) \
-^c#00FA9A^^b#8B8989^$(echo -e "\ue0b0") \
-^c#000000^^b#8B8989^$(dwm_date) \
-"
+    echo -ne "${dwm_status}"
+}
+
+xsetroot -name "$(gen_str)"
 
