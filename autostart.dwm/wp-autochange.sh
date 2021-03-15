222d11819dfe2f5c068acae7ae1ce978f38af95f
sunowsir <sunow.wang@gmail.com>
2021-03-15 14:36:29 +0800

    [update] update wallpaper change script.

--- autostart.dwm/wp-autochange.sh
+++ autostart.dwm/wp-autochange.sh
@@ -5,7 +5,9 @@ sleep 1.5
 while true; do
 	/bin/bash ~/.config/autostart.dwm/wp-change.sh
 
-    # 更换壁纸的同时刷新一下状态栏，不然会出现状态栏不透明的情况
+    # 更换壁纸的后稍晚一些刷新一下状态栏，不然会出现状态栏不透明的情况
+    sleep 0.5
 	bash .~/.config/autostart.dwm/dwm-status-refresh.sh
+
 	sleep 3m
 done
