52e1d06e799c712b24fa8099dba792002267ed3e
sunowsir <sunow.wang@gmail.com>
2021-03-22 19:46:50 +0800

    [change] change dwm autostart script

--- autostart.dwm/config.sh
+++ autostart.dwm/config.sh
@@ -23,18 +23,21 @@ DWMBAR_DATA=(
 # Change the variable setting to your favorite theme
 DWMBAR_THEME="default"
 
+# You don’t need to use ‘&’ to make the command run in the background, 
+# the engine script will automatically run the script you configured in the background.
+
 # Change the array to set the initialization command.
 DWM_INIT_LIST=(
-    'sleep 1; DWM_DISPLAY_set;'
+    'sleep 1 & DWM_DISPLAY_set;'
     'DWM_SETUP_inverse-scroll;'
     'picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b --experimental-backends;'
     'nm-applet;'
-    'sleep 2; fcitx5;'
+    'sleep 2 & fcitx5;'
 )
 
 # Change the array to set timing tasks.
 DWM_TASK_LIST=(
-    'DWMBAR_THEME_Main_Handle "${DWMBAR_THEME}" "${DWMBAR_DATA[@]}"; sleep 1;'
+    'DWMBAR_THEME_Main_Handle "${DWMBAR_THEME}" "${DWMBAR_DATA[@]}" & sleep 1;'
     'feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/ & sleep 3m;'
 )
 
