2f061d6a404226f50664a281462afbffa8da29b2
sunowsir <sunow.wang@gmail.com>
2021-03-23 21:10:52 +0800

    [change] add dwm cmake

---
+++ dwm/build/CMakeFiles/dwm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../bin/dwm"
+  "../bin/dwm.pdb"
+  "CMakeFiles/dwm.dir/drw.c.o"
+  "CMakeFiles/dwm.dir/dwm.c.o"
+  "CMakeFiles/dwm.dir/util.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dwm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
