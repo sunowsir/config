1a00aa01a881146e6a6e728efc94286e1c61a109
sunowsir <sunow.wang@gmail.com>
2021-08-21 23:28:34 +0800

    [change] change battery icon

--- autostart.dwm/informations/dwm_battery.sh
+++ autostart.dwm/informations/dwm_battery.sh
@@ -8,28 +8,31 @@ dwm_battery () {
     # Change BAT1 to whatever your battery is identified as. Typically BAT0 or BAT1
     CHARGE=$(cat /sys/class/power_supply/BAT0/capacity 2> /dev/null)
     STATUS=$(cat /sys/class/power_supply/BAT0/status 2> /dev/null)
+
+    local declare -a battery_logo;
+    battery_logo[0]="";
+    battery_logo[1]="";
+    battery_logo[2]="";
+    battery_logo[3]="";
+    battery_logo[4]="";
+    battery_logo[5]="";
+    battery_logo[6]="";
+    battery_logo[7]="";
+    battery_logo[8]="";
+    battery_logo[9]="";
+    battery_logo[10]="";
+
+    battery_logo[11]="";
     
-    local logo="BAT"
+    local logo_idx="BAT"
     if [[ "$IDENTIFIER" = "unicode" ]] ; then
-
-        logo="ﮣ"
+        logo_idx=11
         if [[ "$STATUS" = "Discharging" ]]; then
-
-            [[ ${CHARGE} -gt 0 ]] && [[ ${CHARGE} -le 10 ]] &&
-                logo="";
-            [[ ${CHARGE} -gt 10 ]] && [[ ${CHARGE} -le 40 ]] &&
-                logo="";
-            [[ ${CHARGE} -gt 40 ]] && [[ ${CHARGE} -le 60 ]] &&
-                logo="";
-            [[ ${CHARGE} -gt 60 ]] && [[ ${CHARGE} -le 95 ]] &&
-                logo=""; 
-            [[ ${CHARGE} -gt 95 ]] &&
-                logo=""
-
+            logo_idx=$((CHARGE / 10))
         fi
     fi
 
-    echo -en "${logo} ${CHARGE:?}%"
+    echo -en "${battery_logo[${logo_idx}]} ${CHARGE:?}%"
     return "${?}"
 }
 
