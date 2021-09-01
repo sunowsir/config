154e394d1b5ab30455baa76efafea3bd355881f7
sunowsir <sunow.wang@gmail.com>
2021-09-01 16:16:42 +0800

    [add] add nvm config

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -2,5 +2,6 @@ source "${HOME}/.config/zsh/source.zsh"
 
 source "${HOME}/.config/zsh/persional.config.zsh"
 source "${HOME}/.config/zsh/fzf.config.zsh"
+source "${HOME}/.config/zsh/nvm.config.zsh"
 source "${HOME}/.config/zsh/zinit.config.zsh"
 
